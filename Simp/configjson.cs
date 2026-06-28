6153e3fd5809dafa2383e117c15d0dda3e79a9fd
Daniel Bulman <dbulman68@gmail.com>
2026-06-28 16:58:52 +0100

    Removed command prefix from SIMP configjson

--- Simp/configjson.cs
+++ Simp/configjson.cs
@@ -6,7 +6,5 @@ namespace Simp
     {
         [JsonProperty(nameof(Token))]//gets token from json file sets it to store in token
         public string Token { get; private set; }
-        [JsonProperty(nameof(CommandPrefix))]//gets prefix from file stores it to prefix
-        public string CommandPrefix { get; private set; }
     }
 }
